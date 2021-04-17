48fca2fd03c31c1bb531ce52d5334f9740bcb97d
seldeleren <82264656+arinaaas@users.noreply.github.com>
2021-04-17 13:27:56 +0300

    vvvvvvvvvv 1.222222

--- ping pong jf.py
+++ ping pong jf.py
@@ -1,5 +1,17 @@
 from pygame import*
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 back = (2,250, 172)
 win_widht = 700
 win_height = 600
